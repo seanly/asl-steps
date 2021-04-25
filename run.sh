b594f032c82df9842b96198b441696166159c132
Sean Ly <seanly@aliyun.com>
2021-04-25 23:35:51 +0800

    [feat] 添加运行task的脚本，简化执行命令

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#set -eux
+set -eu
+
+WORKSPACE=$(pwd)
+echo "--//INFO: ws.dir: ${WORKSPACE}"
+
+ASL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ANT_HOME=${ASL_DIR}/tools/ant
+_ANT_EXEC=${ANT_HOME}/bin/ant
+
+TASKS_DIRS=${ASL_DIR}/tasks
+
+if [ -n "$1" ]; then
+  TASK=$1
+  if [ ! -d "${TASKS_DIRS}/${TASK}" ]; then
+    echo "--//INFO: please check task.name"
+    exit -1
+  fi
+  shift
+else
+  echo "--//INFO: please set task.name"
+  exit -1
+fi
+
+
+RUN_BUILDFILE=${ASL_DIR}/run.xml
+exec ${_ANT_EXEC} -f ${RUN_BUILDFILE} -logger org.apache.tools.ant.NoBannerLogger -Dasl.task.id=${TASK} $@
