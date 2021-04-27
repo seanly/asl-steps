0af44d1f8efb33a174dd400690cce6c8756ab47e
Sean Ly <seanly@aliyun.com>
2021-04-27 15:23:34 +0800

    [feat] 镜像默认启动dockerd服务

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,11 +6,16 @@ do
     echo "${K}=\"${VAL}\"" >> /etc/envvars
 done
 
+function start_dockerd() {
+  dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --iptables=false --ip-masq=false > /dev/null 2>&1 &
+}
+
 if [ $# -eq 0 ]; then
     exec /usr/bin/runsvinit
 fi
 
 [ "$1" == '--' ] && shift
+start_dockerd
 exec $@
 
     
