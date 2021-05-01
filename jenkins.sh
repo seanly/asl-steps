7159b8921a2848d35d12d7be5d935821e4e5baaa
Sean Ly <seanly@aliyun.com>
2021-05-01 16:22:20 +0800

    [fixbug] 添加错误信息展示

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-if [ -f /etc/redhat-release ]; then
-  cat /etc/redhat-release
+build() {
+  echo "--//INFO: build...."
+}
+
+function test() {
+  echo "--//INFO: test...."
+  if [ -f /etc/redhat-release ]; then
+    cat /etc/redhat-release
+  fi
+  echo 'hi, docker'
+}
+
+function package() {
+  echo "--// package ...."
+}
+
+if [ "$#" == 0 ]; then
+  echo "--//ERR: please set function name"
+  exit 2
 fi
 
-pwd
 
-echo 'hi, docker'
+# ---不可变部分----
+fname=`declare -f -F $1`
+if [ -n "$fname" ]; then
+  $@
+else 
+  echo 1
+fi
+
+status=$?
+
+# **注意** 修改工作空间权限，避免Jenkins任务权限问题
+if [[ -n "$DAPPER_UID" && -n "$DAPPER_GID" ]]; then
+  chown -R $DAPPER_UID:$DAPPER_GID .
+fi
+
+exit $status
+
--- run.sh
+++ run.sh
@@ -10,19 +10,59 @@ ASL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ANT_HOME=${ASL_DIR}/tools/ant
 _ANT_EXEC=${ANT_HOME}/bin/ant
 
-TASKS_DIRS=${ASL_DIR}/tasks
+RUN_BUILDFILE=${ASL_DIR}/run.xml
 
-if [ -n "$1" ]; then
-  TASK=$1
-  if [ ! -d "${TASKS_DIRS}/${TASK}" ]; then
-    echo "--//INFO: please check task.name"
+
+task() {
+
+  TASKS_DIRS=${ASL_DIR}/tasks
+  if [ -n "$1" ]; then
+    TASK=$1
+    if [ ! -d "${TASKS_DIRS}/${TASK}" ]; then
+      echo "--//INFO: please check task.name"
+      exit -1
+    fi
+    shift
+  else
+    echo "--//INFO: please set task.name"
     exit -1
   fi
-  shift
-else
-  echo "--//INFO: please set task.name"
-  exit -1
+
+  exec ${_ANT_EXEC} -f ${RUN_BUILDFILE} task \
+    -logger org.apache.tools.ant.NoBannerLogger -Dtask.id=${TASK} $@
+}
+
+pipeline() {
+  if [ -n "$1" ]; then
+    PIPELINE_FILE=$1
+    if [ ! -f "${PIPELINE_FILE}" ]; then
+      echo "--//INFO: please check pipeline.file"
+      exit -1
+    fi
+    shift
+  else
+    echo "--//INFO: please set pipeline.file"
+    exit -1
+  fi
+  exec ${_ANT_EXEC} -f ${RUN_BUILDFILE} pipeline \
+    -logger org.apache.tools.ant.NoBannerLogger \
+    -Dpipeline.file=$PIPELINE_FILE $@
+}
+
+# ---不可变部分----
+fname=`declare -f -F $1`
+if [ -n "$fname" ]; then
+  $@
+else 
+  echo 1
 fi
 
-RUN_BUILDFILE=${ASL_DIR}/run.xml
-exec ${_ANT_EXEC} -f ${RUN_BUILDFILE} task -logger org.apache.tools.ant.NoBannerLogger -Dtask.id=${TASK} $@
+status=$?
+
+# **注意** 修改工作空间权限，避免Jenkins任务权限问题
+if [[ -n "$DAPPER_UID" && -n "$DAPPER_GID" ]]; then
+  chown -R $DAPPER_UID:$DAPPER_GID .
+fi
+
+exit $status
+
